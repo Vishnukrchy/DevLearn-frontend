a4847a43303bcf87fabf74f0db68a183dc37d2eb
Vishnu Kumar <161946782+Vishnukrchy@users.noreply.github.com>
2024-11-29 11:00:53 +0530

    ccd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Github, Twitter, Linkedin, Code2, ExternalLink } from 'lucide-react';
   //  Footer
-function Footer() 
+function Footer() {}
   return (
     <footer className="bg-gray-900">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
